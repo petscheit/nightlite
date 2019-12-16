9d9fea9f0a36723ac83e28b20f41b76f8dddf330
Will Kim <william.kim@ey.com>
2019-12-16 15:10:44 -0500

    feat: add merkleTree

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const generateZokratesFiles = require('./setup/generateZokratesFiles');
+const merkleTree = require('./merkleTree');
 const provider = require('./provider');
 const vks = require('./vks');
 
 module.exports = {
   generateZokratesFiles,
+  merkleTree,
   provider,
   vks,
 };
